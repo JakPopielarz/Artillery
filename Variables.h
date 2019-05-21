7ab4e0e90957b5cbdb58586aae7780bc445fdb9b
JakPopielarz <popielarzjakub7@gmail.com>
2019-05-21 12:05:41 +0200

    Defined global window variables, made terrain as convex shape

---
+++ Variables.h
@@ -0,0 +1,13 @@
+//
+// Created by jakub on 21.05.19.
+//
+
+#ifndef ARTILLERY_VARIABLES_H
+#define ARTILLERY_VARIABLES_H
+
+unsigned int WINDOW_WIDTH = 800;
+unsigned int WINDOW_HEIGHT = 600;
+int TERRAIN_POINT_COUNT = 4;
+float TERRAIN_POINT_COEFF = WINDOW_WIDTH / TERRAIN_POINT_COUNT;
+
+#endif //ARTILLERY_VARIABLES_H
--- main.cpp
+++ main.cpp
@@ -2,21 +2,25 @@
 #include <SFML/Graphics.hpp>
 #include "Point.h"
 #include "Cannon.h"
+#include "Variables.h"
 
 using namespace std;
 
 int main() {
-    Point point = Point(2, 14314.4f);
-    cout << point << endl;
-    Point point2 = Point(-2, -14314);
-    point += point2;
-    cout << point << endl;
-    Cannon cannon = Cannon(point);
-    cout << cannon.get_hp() << ", " << cannon.get_position() << endl;
+    float terrain_points[] = {0.f, 100.f, 200.f, 300.f, 400.f};
 
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Artillery");
+    sf::ConvexShape terrain(6);
+    terrain.setFillColor(sf::Color(35, 147, 67));
+    terrain.setOutlineColor(sf::Color::Black);
+    terrain.setOutlineThickness(-1);
+
+    for (int i=0; i<=TERRAIN_POINT_COUNT; i++) {
+        terrain.setPoint(i, sf::Vector2f(i*TERRAIN_POINT_COEFF, WINDOW_HEIGHT-terrain_points[i]));
+    }
+
+    terrain.setPoint(TERRAIN_POINT_COUNT+1, sf::Vector2f(WINDOW_WIDTH, WINDOW_HEIGHT));
+    terrain.setPoint(TERRAIN_POINT_COUNT+2, sf::Vector2f(0, WINDOW_HEIGHT));
 
     while (window.isOpen())
     {
@@ -27,9 +31,10 @@ int main() {
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        window.clear(sf::Color(139, 194, 239));
+        window.draw(terrain);
         window.display();
     }
+
     return 0;
 }
