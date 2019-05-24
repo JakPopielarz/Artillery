571181302d555eb69ca98f6eb5370c725efdfd8b
filipszoldra <37276160+filipszoldra@users.noreply.github.com>
2019-05-24 16:11:17 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,16 +1,41 @@
-#include<iostream>
-#include<array>
-#include<vector>
-#include<math.h>
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "Point.h"
+#include "Cannon.h"
+
 using namespace std;
 bool missile_in_the_air(float missile_x, float missile_y, float terrain[]){ //terrrain to tablica terenu, zmieniê nazwy zmiennych jak bêdzie wrzucony missile
 	if(missile_y>terrain[missile_x])
 		return true;
 	return false;
 }
-int main
-{
-	//trzeba pobraæ z obiektu px, py, vx i vy
+int main() {
+    Point point = Point(2, 14314.4f);
+    cout << point << endl;
+    Point point2 = Point(-2, -14314);
+    point += point2;
+    cout << point << endl;
+    Cannon cannon = Cannon(point);
+    cout << cannon.get_hp() << ", " << cannon.get_position() << endl;
+
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    //trzeba pobraæ z obiektu px, py, vx i vy
 	float gravity = 9.81; // 9,81m/s^2
 	float delta_time = 0.01; // 0,01s
 	float mis_position[400]; //tablica zapisuj¹ca tor ruchu pocisku
@@ -26,5 +51,5 @@ int main
 			current_x++;
 		}
 		}
-	
+    return 0;
 }
