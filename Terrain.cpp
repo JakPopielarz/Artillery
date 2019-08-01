b5a4954fb9656bdaa19403a928cca9f6baaa8599
JakPopielarz <popielarzjakub7@gmail.com>
2019-08-01 14:06:32 +0200

    Generating terrain based on a sine

--- Terrain.cpp
+++ Terrain.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 #include "Terrain.h"
 
 
 unsigned int WIDTH = 800;
 unsigned int HEIGHT = 600;
 
-Terrain::Terrain() {
-    sf::VertexArray vertices(sf::LineStrip, 800);
+Terrain::Terrain(float density, sf::Color outline_color) {
+    int number_of_vertices = WIDTH*density +1 ;
+    sf::VertexArray vertices(sf::LineStrip, number_of_vertices);
 
-    vertices[0].position = sf::Vector2f(0, HEIGHT-70);
-    vertices[0].color = sf::Color::Red;
-    for(int i=1; i<800; i++) {
-        int offset = (rand() % (5 - 1 + 1) + 1) * (rand() % (1 + 1 + 1) - 1);
-        vertices[i].position = sf::Vector2f(i, vertices[i - 1].position.y + offset);
-        vertices[i].color = sf::Color::Red;
+    int mod = rand() % (150 - 80 + 1) + 80;
+    float phase = ((float) rand()) / (float) 2;
+    for(int i=1; i<number_of_vertices; i++) {
+        vertices[i].position = sf::Vector2f(i/density, HEIGHT-mod - mod*float(sin(phase*M_1_PI + (i * mod*0.0001))));
+        vertices[i].color = outline_color;
         if (vertices[i].position.y > HEIGHT)
-            vertices[i].position.y = HEIGHT - 10;
+            vertices[i].position.y = HEIGHT;
         if (vertices[i].position.y < 0)
-            vertices[i].position.y = 10;
+            vertices[i].position.y = 0;
     }
-    
+
+    vertices[0].position = sf::Vector2f(0, vertices[1].position.y);
+    vertices[0].color = outline_color;
+
     setVertices(vertices);
 }
 
--- Terrain.h
+++ Terrain.h
@@ -7,7 +7,7 @@ class Terrain{
 private:
     sf::VertexArray terrain;
 public:
-    Terrain();
+    explicit Terrain(float density=1, sf::Color outline_colour=sf::Color::Black);
     void setVertices(sf::VertexArray &vertices);
     void draw(sf::RenderWindow& window);
 };
