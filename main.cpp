89cae917e38980a607548d69cc94b2cad8ec2fe9
JakPopielarz <popielarzjakub7@gmail.com>
2019-08-02 11:53:13 +0200

    cleaning up main.cpp of unused functions (split(), values())

--- main.cpp
+++ main.cpp
@@ -9,47 +9,6 @@
 
 using namespace std;
 
-vector<string> split(string& str) {
-    vector<string> split_vc = vector<string>();
-    size_t pos;
-    while(!str.empty()) {
-        pos = str.find(" ");
-        split_vc.emplace_back(str.substr(0, pos));
-        str.erase(0, pos);
-        if (str.find(" ") == 0)
-            str.erase(0, 1);
-    }
-    return split_vc;
-}
-
-vector<float> values(string function) {
-    vector<string> f_split_str = split(function);
-    size_t x_pos;
-    string x_str_exp;
-    float x_exp=0, coeff=0, value=0, x=0;
-    vector<float> values = vector<float>();
-
-    for (int k=0; k<800; k+=2) {
-        x = float(k)/100.0f;
-        for (string i: f_split_str) {
-            x_pos = i.find("x");
-
-            if (x_pos != string::npos) {
-                x_str_exp = i.substr(x_pos + 2);
-                i.erase(x_pos);
-                x_exp = stof(x_str_exp);
-            } else {
-                x_exp = 0;
-            }
-
-            coeff = stof(i);
-            value += coeff * float(pow(x, x_exp)) + 1;
-        }
-        values.push_back(value);
-    }
-    return values;
-}
-
 int main() {
     srand(time(NULL));
 
@@ -73,6 +32,7 @@ int main() {
 
         window.clear(sf::Color(139, 194, 239));
         terrain.draw(window);
+        cannon.draw(window);
         window.display();
     }
 
